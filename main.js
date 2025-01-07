e0a0284f569dbe645cb70b2028258fdbd421a5c1
Kim Khanh Do <kim.do2709@gmail.com>
2025-01-07 11:58:23 +1100

    Add the feature "Edit task"

--- main.js
+++ main.js
@@ -8,6 +8,24 @@ const taskList = document.querySelector('#task-list');
 const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-input');
 
+function handleTaskActions(e) {
+    const taskItem = e.target.closest('.task-item');
+    const taskIndex = +taskItem.getAttribute('task-index');
+    const task = tasks[taskIndex];
+
+    if (e.target.closest('.edit')) {
+        const newTitle = prompt('Enter new task title:', task.title);
+        task.title = newTitle;
+        renderTasks();
+    }
+    if (e.target.closest('.done')) {
+        console.log('Done');
+    }
+    if (e.target.closest('.delete')) {
+        console.log('Delete');
+    }
+}
+
 function addNewTask(e) {
     e.preventDefault();
 
@@ -27,9 +45,9 @@ function addNewTask(e) {
 function renderTasks() {
     const liItems = tasks
         .map(
-            (task) =>
+            (task, index) =>
                 `
-    <li class="task-item ${task.completed ? 'completed' : ''}">
+    <li class="task-item ${task.completed ? 'completed' : ''}" task-index='${index}'>
     <span class="task-title">${task.title}</span>
     <div class="task-action">
         <button class="task-btn edit">Edit</button>
@@ -43,6 +61,6 @@ function renderTasks() {
     taskList.innerHTML = liItems;
 }
 
-// todoForm.addEventListener('submit', handleTaskAction);
-todoForm.addEventListener('submit', addTask);
 renderTasks();
+todoForm.addEventListener('submit', addNewTask);
+taskList.addEventListener('click', handleTaskActions);
